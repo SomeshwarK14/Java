2fbedcb5258ce4ec82e1010918a3610d99c78c67
Someshwar K <warsomesh22@gmail.com>
2025-01-08 20:49:50 +0530

    Size of Primitive DataTypes

---
+++ PrimitiveDateTypeSize/src/PrimDTsize/PrimDTS.java
@@ -0,0 +1,17 @@
+package PrimDTsize;
+
+public class PrimDTS {
+	public static void main(String[] args) {
+		System.out.println("Size of Primitive Datetypes");
+		System.out.println("The size of the byte is " + Byte.BYTES + " bytes ");
+		System.out.println("The size of the int is " + Integer.BYTES + " bytes ");
+		System.out.println("The size of the char is " + Character.BYTES + " bytes ");
+		System.out.println("The size of the short is " + Short.BYTES + " bytes ");
+		System.out.println("The size of the long is " + Long.BYTES + " bytes ");
+		System.out.println("The size of the float is " + Float.BYTES + " bytes ");
+		System.out.println("The size of the double is " + Double.BYTES + " bytes ");
+		System.out.println("The size of the boolean is " + 1 + " bit (Typically) ");
+
+	}
+
+}
