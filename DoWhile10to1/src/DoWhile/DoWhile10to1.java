8bc160ce7a704bb457ed077a41836fad8779e87b
Someshwar K <warsomesh22@gmail.com>
2025-01-09 20:57:29 +0530

    Add files via upload

---
+++ DoWhile10to1/src/DoWhile/DoWhile10to1.java
@@ -0,0 +1,15 @@
+package DoWhile;
+
+public class DoWhile10to1 {
+
+	public static void main(String[] args) {
+		int i=10;
+		do 
+		{
+			System.out.println(i);
+			i--;
+		}
+		while(i>=1);
+	}
+
+}
